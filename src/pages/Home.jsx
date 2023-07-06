007baeff25f29bca387cc6b5218fd1f784aa9599
Ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2023-07-06 17:26:56 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,6 +61,14 @@ class Home extends Component {
         </div>
         <button className="prev-button" onClick={this.prevSlide}>Previous</button>
         <button className="next-button" onClick={this.nextSlide}>Next</button>
+
+        <div className='home-about-section'>
+          <h2 className="home-about-section-title">About Us</h2>
+          <p className="home-about-section-description">Heeds Foundation, founded by students from Nagpur City, is a non-profit organization dedicated to empowering communities through education and addressing social issues and instabilities. with a mission to promote communities through education, healthcare, sustainable development, and humanitarian response, Heeds Foundation aims to promote support, resources, and assistance to those in need. The organization was established by a group of passionate students who identified various social challenges and decided to take action by forming a Heeds Foundation. Through their collective efforts, Heeds Foundation strives to create positive change, promote equality, and make a meaningful impact in the lives of individuals and communities.
+
+          <br></br>
+          Our primary focus is on promoting quality education to underserved children, ensuring that every child has access to learning opportunities. We believe that education is the key to unlocking a brighter future and creating lasting change. Through our initiatives, we aim to bridge the educational gap, promote resources and scholarships, and promote equal access to education for all. With a strong commitment to promoting equality and fostering positive change, Heeds Foundation strives to empower individuals and communities through the transformative power of education.</p>
+        </div>
       </div>
     );
   }
