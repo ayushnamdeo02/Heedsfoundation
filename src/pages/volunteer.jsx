6a7dd7c74b2706a20ad27d8611031372c0cb95f7
Ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2023-07-07 04:55:46 +0530

    old

--- src/pages/volunteer.jsx
+++ src/pages/volunteer.jsx
@@ -3,47 +3,31 @@ import axios from 'axios';
 import './Volunteer/Volunteer.css';
 import volunteer_head from './assets/head.png';
 
+
 class Volunteer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       fullName: '',
-      emailid: '',
+      emailid:'',
       role: 'volunteer',
       gender: '',
       dateOfBirth: '',
       contactNo: '',
       address: '',
       agreed: '',
-      emailValid: true,
-      mobileNumberValid: true,
     };
   }
 
   handleSubmit = async (e) => {
     e.preventDefault();
 
-    const {
-      agreed,
-      fullName,
-      role,
-      gender,
-      dateOfBirth,
-      contactNo,
-      address,
-      emailid,
-      emailValid,
-      mobileNumberValid,
-    } = this.state;
+    const { agreed, fullName, role, gender, dateOfBirth, contactNo, address,emailid } =
+      this.state;
 
     if (agreed !== 'Yes') {
       alert('Please agree to the declaration before submitting.');
-      return;
-    }
-
-    if (!emailValid || !mobileNumberValid) {
-      alert('Please enter valid email and mobile number.');
       return;
     }
 
@@ -69,14 +53,12 @@ class Volunteer extends Component {
         this.setState({
           fullName: '',
           role: 'volunteer',
-          emailid: '',
+          emailid:'',
           gender: '',
           dateOfBirth: '',
           contactNo: '',
           address: '',
           agreed: 'Yes',
-          emailValid: true,
-          mobileNumberValid: true,
         });
       } else {
         alert('Registration submission failed. Please try again.');
@@ -89,25 +71,7 @@ class Volunteer extends Component {
 
   handleInputChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value }, () => {
-      if (name === 'emailid') {
-        this.validateEmail(value);
-      } else if (name === 'contactNo') {
-        this.validateMobileNumber(value);
-      }
-    });
-  };
-
-  validateEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const isValid = emailRegex.test(email);
-    this.setState({ emailValid: isValid });
-  };
-
-  validateMobileNumber = (mobileNumber) => {
-    const mobileNumberRegex = /^[0-9]{10}$/;
-    const isValid = mobileNumberRegex.test(mobileNumber);
-    this.setState({ mobileNumberValid: isValid });
+    this.setState({ [name]: value });
   };
 
   render() {
@@ -120,117 +84,116 @@ class Volunteer extends Component {
       contactNo,
       address,
       agreed,
-      emailValid,
-      mobileNumberValid,
     } = this.state;
 
     return (
       <div className="volunteer-container">
-        <img src={volunteer_head} alt="head" />
+        
+        <img src={volunteer_head} alt='head'></img>
+        
         <div className="title-volunteer">Join Heeds Foundation Family</div>
         <form className="volunteer-form" onSubmit={this.handleSubmit}>
           <div className="info-volunteer">
-            <div className="info-title">Core Principles of Heeds Foundation</div>
+            <div className='info-title'>Core Principles of Heeds Foundation</div>
             <ul>
-              <li>
-                <b>Collaboration:</b> We actively seek to collaborate with like-minded organizations, volunteers, and
-                community members to maximize our collective efforts.
-              </li>
-              <li>
-                <b>Equality:</b> We believe in equality and inclusivity for all individuals, irrespective of their
-                background, gender, or socioeconomic status.
-              </li>
-              <li>
-                <b>Volunteering:</b> We recognize the value and impact of volunteers in furthering our mission. We
-                promote a culture of volunteerism.
-              </li>
+              <li><b>Collaboration:</b> We actively seek to collaborate with like-minded organizations, volunteers, and community members to maximize our collective efforts.</li>
+              <li><b>Equality:</b> We believe in equality and inclusivity for all individuals, irrespective of their background, gender, or socioeconomic status.</li>
+              <li><b>Volunteering:</b> We recognize the value and impact of volunteers in furthering our mission. We promote a culture of volunteerism.</li>
             </ul>
           </div>
-          <div className="body-form">
-            <label className="role">Join as:</label>
-            <select id="role" name="role" value={role} onChange={this.handleInputChange}>
-              <option value="volunteer">Volunteer</option>
-              <option value="donor">Donor</option>
-            </select>
+          <div className='body-form'>
+          <label className="role">Join as:</label>
+        <select
+          id="role"
+          name="role"
+          value={role}
+          onChange={this.handleInputChange}
+        >
+          <option value="volunteer">Volunteer</option>
+          <option value="donor">Donor</option>
+        </select>
 
-            <label className="fullName">Full Name:</label>
-            <input
-              type="text"
-              id="fullName"
-              name="fullName"
-              value={fullName}
-              onChange={this.handleInputChange}
-              required
-            />
+        <label className="fullName">Full Name:</label>
+        <input
+          type="text"
+          id="fullName"
+          name="fullName"
+          value={fullName}
+          onChange={this.handleInputChange}
+          required
+        />
+        <label className="emailid">Email id:</label>
+        <input
+          type="text"
+          id="emailid"
+          name="emailid"
+          value={emailid}
+          onChange={this.handleInputChange}
+          required
+        />
 
-            <label className="emailid">Email id:</label>
-            <input
-              type="email"
-              id="emailid"
-              name="emailid"
-              value={emailid}
-              onChange={this.handleInputChange}
-              required
-            />
-            {!emailValid && <div className="error">Please enter a valid email.</div>}
+        <label className="gender">Gender:</label>
+        <select
+          id="gender"
+          name="gender"
+          value={gender}
+          onChange={this.handleInputChange}
+        >
+          <option value="">Select Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Other">Other</option>
+        </select>
 
-            <label className="gender">Gender:</label>
-            <select id="gender" name="gender" value={gender} onChange={this.handleInputChange}>
-              <option value="">Select Gender</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Other">Other</option>
-            </select>
+        <label className="dateOfBirth">Date of Birth:</label>
+        <input
+          type="text"
+          id="dateOfBirth"
+          name="dateOfBirth"
+          value={dateOfBirth}
+          onChange={this.handleInputChange}
+          required
+        />
 
-            <label className="dateOfBirth">Date of Birth:</label>
-            <input
-              type="text"
-              id="dateOfBirth"
-              name="dateOfBirth"
-              value={dateOfBirth}
-              onChange={this.handleInputChange}
-              required
-            />
+        <label className="contactNo">Contact No:</label>
+        <input
+          type="tel"
+          id="contactNo"
+          name="contactNo"
+          value={contactNo}
+          onChange={this.handleInputChange}
+          required
+        />
 
-            <label className="contactNo">Contact No:</label>
-            <input
-              type="tel"
-              id="contactNo"
-              name="contactNo"
-              value={contactNo}
-              onChange={this.handleInputChange}
-              required
-            />
-            {!mobileNumberValid && <div className="error">Please enter a valid 10-digit mobile number.</div>}
+        <label className="address">Address:</label>
+        <textarea
+          id="address"
+          name="address"
+          value={address}
+          onChange={this.handleInputChange}
+          required
+        ></textarea>
 
-            <label className="address">Address:</label>
-            <textarea
-              id="address"
-              name="address"
-              value={address}
-              onChange={this.handleInputChange}
-              required
-            ></textarea>
+        <div className="declaration">
+          <label className="agreed">
+            I agree to follow the Heeds Foundation core principles and I
+            understand that any personal risk during the process will be borne
+            by me and the Heeds Foundation is not liable.
+          </label>
+          <select
+            id="agreed"
+            name="agreed"
+            value={agreed}
+            onChange={this.handleInputChange}
+            required
+          >
+            <option value="">Select</option>
+            <option value="Yes">Yes</option>
+          </select>
+        </div>
 
-            <div className="declaration">
-              <label className="agreed">
-                I agree to follow the Heeds Foundation core principles and I understand that any personal risk during the
-                process will be borne by me and the Heeds Foundation is not liable.
-              </label>
-              <select
-                id="agreed"
-                name="agreed"
-                value={agreed}
-                onChange={this.handleInputChange}
-                required
-              >
-                <option value="">Select</option>
-                <option value="Yes">Yes</option>
-              </select>
-            </div>
-
-            <button type="submit">Submit</button>
-          </div>
+        <button type="submit">Submit</button>
+        </div>
         </form>
       </div>
     );
