f4d3de7e05f3c2e8ced456bdc36cf8a9d3462e3c
Ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2023-07-07 16:37:28 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Helmet } from 'react-helmet';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Helmet>
-      <title>Heeds-Foundation | Home</title>
-      <meta name="description" content="Welcome to Heeds Foundation - a platform for volunteering, making a difference, and creating a positive impact. Join us today!" />
-    </Helmet>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
