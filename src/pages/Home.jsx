76491b0c473b413532869bcc1e8745e504665161
Ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2023-07-07 15:33:58 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 import "./home/home.css";
 import image1 from '../images/slider-1.jpg';
 import image2 from "../images/slider-2.jpg";
@@ -8,11 +9,9 @@ import image5 from "../images/slider-5.jpg";
 import image6 from "../images/slider-6.jpg";
 import image7 from "../images/slider-7.jpg";
 
-
-
 class Home extends Component {
   state = {
-    images: [image1, image2, image3, image4, image5, image6,image7],
+    images: [image1, image2, image3, image4, image5, image6, image7],
     currentSlide: 0
   };
 
@@ -47,8 +46,29 @@ class Home extends Component {
   render() {
     const { images, currentSlide } = this.state;
 
+    const structuredData = {
+      '@context': 'https://schema.org',
+      '@type': 'Organization',
+      name: 'Heeds Foundation',
+      description: 'A non-profit organization empowering communities through education',
+      logo: 'https://heedsfoundation.com/',
+      url: 'https://heedsfoundation.com/',
+      contactPoint: {
+        '@type': 'ContactPoint',
+        telephone: '+91 8788411441',
+        email: 'heedsfoundation.ngo@gmail.com',
+        contactType: 'customer support',
+      },
+    };
+
     return (
       <div className='container-home'>
+        <Helmet>
+          <title>Heeds Foundation - Empowering Communities through Education</title>
+          <meta name="description" content="Heeds Foundation is a non-profit organization dedicated to empowering communities through education and addressing social issues. Learn more about our mission and initiatives." />
+          <script type="application/ld+json">{JSON.stringify(structuredData)}</script>
+        </Helmet>
+
         <div className='slider'>
           {images.map((image, index) => (
             <img
@@ -64,10 +84,9 @@ class Home extends Component {
 
         <div className='home-about-section'>
           <h2 className="home-about-section-title">About Us</h2>
-          <p className="home-about-section-description">Heeds Foundation, founded by students from Nagpur City, is a non-profit organization dedicated to empowering communities through education and addressing social issues and instabilities. with a mission to promote communities through education, healthcare, sustainable development, and humanitarian response, Heeds Foundation aims to promote support, resources, and assistance to those in need. The organization was established by a group of passionate students who identified various social challenges and decided to take action by forming a Heeds Foundation. Through their collective efforts, Heeds Foundation strives to create positive change, promote equality, and make a meaningful impact in the lives of individuals and communities.
-
-          <br></br>
-          Our primary focus is on promoting quality education to underserved children, ensuring that every child has access to learning opportunities. We believe that education is the key to unlocking a brighter future and creating lasting change. Through our initiatives, we aim to bridge the educational gap, promote resources and scholarships, and promote equal access to education for all. With a strong commitment to promoting equality and fostering positive change, Heeds Foundation strives to empower individuals and communities through the transformative power of education.</p>
+          <p className="home-about-section-description">Heeds Foundation, founded by students from Nagpur City, is a non-profit organization dedicated to empowering communities through education and addressing social issues and instabilities. With a mission to promote communities through education, healthcare, sustainable development, and humanitarian response, Heeds Foundation aims to provide support, resources, and assistance to those in need. The organization was established by a group of passionate students who identified various social challenges and decided to take action by forming Heeds Foundation. Through their collective efforts, Heeds Foundation strives to create positive change, promote equality, and make a meaningful impact in the lives of individuals and communities.
+            <br /><br />
+            Our primary focus is on promoting quality education for underserved children, ensuring that every child has access to learning opportunities. We believe that education is the key to unlocking a brighter future and creating lasting change. Through our initiatives, we aim to bridge the educational gap, promote resources and scholarships, and ensure equal access to education for all. With a strong commitment to promoting equality and fostering positive change, Heeds Foundation empowers individuals and communities through the transformative power of education.</p>
         </div>
       </div>
     );
