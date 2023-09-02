e00f0c02ff37233ea8b10a66ba277e6d0c5c68ea
Kartik12b <84952572+Kartik12b@users.noreply.github.com>
2023-09-03 02:44:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './footer/footer.css';
+import privacy from './PrivacyPolicy_heedsfoundation.pdf'
+import TnC from './Termsandcondition_heeedsfoundation.pdf'
+import FAQ from './FAQ_Heedsfoundation.pdf'
 
 class Footer extends Component {
   render() {
@@ -8,9 +11,9 @@ class Footer extends Component {
       <div className="footer-container">
         <div className="left-col">
           <ul>
-            <li className="Privacy-policy">Privacy Policy</li>
-            <li className='Terms-of-use'>Terms Of Use</li>
-            <li className='Contact-us'>Contact Us</li>
+            <li className="Privacy-policy"><a href={privacy}>Privacy Policy</a></li>
+            <li className='Terms-of-use'><a href={TnC}>Terms Of Use</a></li>
+            <li className='Contact-us'><a href={FAQ}>FAQ</a></li>
           </ul>
           </div>
           <div className='right-col'>
